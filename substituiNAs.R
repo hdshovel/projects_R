f3dd409c2d4e426056cd5c810b27044bf082096c
Carlos <xztonet@gmail.com>
2023-01-20 14:54:26 -0300

    Update substituiNAs.R

--- substituiNAs.R
+++ substituiNAs.R
@@ -1,3 +1,13 @@
+'
+Durante o carregamento do dataset, fiz a correcao dos nomes das colunas com clean_names, dividi a coluna hora_local em 2 colunas de hr e min e substituir 
+os NAs da coluna hr por 0 e os NAs da coluna t_ar1 pela media da propria coluna.
+'
+
+'
+While loading the dataset, I corrected the column names with clean_names, divided the local_time column into 2 columns of hr and min and replaced 
+the NAs of the hr column with 0 and the NAs of the t_ar1 column with the average of the column itself.
+'
+
 rm(list = ls())
 
 library(magrittr)
